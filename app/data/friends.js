080d6d8945e856e6a95abeeeec6c6692ee8a8b42
joens16 <joens16@hotmail.com>
2018-08-01 01:51:20 -0500

    fixed pictures

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,91 +1,91 @@
 var friends = [{
     name: "Emily",
-    photo: "https://en.wikipedia.org/wiki/Emily_Ratajkowski#/media/File:2_minutes_with_Emily_Ratajkowski_(2).jpg",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/1/1d/2_minutes_with_Emily_Ratajkowski_%282%29.jpg",
     scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
 },
 {
     name: "Katy",
-    photo: "https://en.wikipedia.org/wiki/Katy_Perry#/media/File:Katy_Perry_at_Madison_Square_Garden_(37436531092)_(cropped).jpg",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/8/8a/Katy_Perry_at_Madison_Square_Garden_%2837436531092%29_%28cropped%29.jpg",
     scores: [1, 4, 4, 4, 3, 1, 5, 5, 4, 3]
 },
 {
     name: "Jessica",
-    photo: "https://en.wikipedia.org/wiki/Jessica_Alba#/media/File:Jessica_Alba_at_TechCrunch_Disrupt_San_Francisco_2012_02_(cropped).jpg",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Jessica_Alba_at_TechCrunch_Disrupt_San_Francisco_2012_02_%28cropped%29.jpg/1920px-Jessica_Alba_at_TechCrunch_Disrupt_San_Francisco_2012_02_%28cropped%29.jpg",
     scores: [1, 5, 2, 2, 1, 5, 4, 1, 2, 5]
 },
 {
     name: "Natalie",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/7/7e/Natalie_Portman_Cannes_2015_5_%28cropped%29.jpg",
     scores: [1, 2, 3, 4, 5, 5, 4, 3, 2, 1]
 },
 {
-    name: "Fiorella",
-    photo: "https://en.wikipedia.org/wiki/Natalie_Portman#/media/File:Natalie_Portman_Cannes_2015_5_(cropped).jpg",
+    name: "Whoopi",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/Whoopi_Goldberg_at_a_NYC_No_on_Proposition_8_Rally.jpg/1280px-Whoopi_Goldberg_at_a_NYC_No_on_Proposition_8_Rally.jpg",
     scores: [2, 4, 1, 3, 5, 2, 4, 1, 3, 5]
 },
 {
     name: "Halle",
-    photo: "https://en.wikipedia.org/wiki/Halle_Berry#/media/File:Halle_Berry_by_Gage_Skidmore_2.jpg",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Halle_Berry_by_Gage_Skidmore_2.jpg/1280px-Halle_Berry_by_Gage_Skidmore_2.jpg",
     scores: [5, 3, 1, 4, 2, 5, 3, 1, 4, 2]
 },
 {
     name: "Giselle",
-    photo: "https://en.wikipedia.org/wiki/Gisele_B%C3%BCndchen#/media/File:Gisele_Bundchen-012_(16251942066).jpg",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/b/b1/Gisele_Bundchen5.jpg",
     scores: [3, 1, 4, 2, 1, 2, 2, 1, 2, 5]
 },
 {
     name: "Rachell",
-    photo: "https://en.wikipedia.org/wiki/Rachel_McAdams#/media/File:Rachel_McAdams_by_Gage_Skidmore.jpg",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Rachel_McAdams_by_Gage_Skidmore.jpg/1280px-Rachel_McAdams_by_Gage_Skidmore.jpg",
     scores: [5, 4, 3, 1, 2, 1, 2, 5, 4, 3]
 },
 {
     name: "Kim",
-    photo: "https://en.wikipedia.org/wiki/Kim_Kardashian#/media/File:Kim_Kardashian_West_2014.jpg",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/3/3e/Kim_Kardashian_5%2C_2012.jpg",
     scores: [2, 2, 3, 3, 4, 4, 5, 5, 1, 1]
 },
 {
     name: "Miley",
-    photo: "https://en.wikipedia.org/wiki/Miley_Cyrus#/media/File:Miley2014.png",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/a/a2/Miley2014.png",
     scores: [1, 2, 5, 4, 1, 5, 1, 4, 4, 5]
 },
 {
     name: "Wilmer",
-    photo: "https://en.wikipedia.org/wiki/Wilmer_Valderrama#/media/File:Wilmer_Valderrama_(7492358912).jpg",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Wilmer_Valderrama_%287492358912%29.jpg/1280px-Wilmer_Valderrama_%287492358912%29.jpg",
     scores: [4, 5, 5, 1, 1, 2, 2, 3, 3, 4]
 },
 {
-    name: "Josh",
-    photo: "https://en.wikipedia.org/wiki/Josh_Duhamel#/media/File:Josh_Duhamel_SXSW_2017_(cropped).jpg",
+    name: "Nicholas",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/c/c0/Nicolas_Cage_Deauville_2013.jpg",
     scores: [3, 4, 4, 5, 5, 1, 1, 2, 2, 3]
 },
 {
     name: "Eddie",
-    photo: "https://en.wikipedia.org/wiki/Eddie_Murphy#/media/File:Eddie_Murphy_by_David_Shankbone.jpg",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/5/5f/Eddie_Murphy_by_David_Shankbone.jpg",
     scores: [1, 2, 2, 3, 3, 4, 4, 5, 5, 1]
 },
 {
     name: "Tom",
-    photo: "https://en.wikipedia.org/wiki/Tom_Cruise#/media/File:Tom_Cruise_by_Gage_Skidmore.jpg",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/7/71/Tom_Cruise_avp_2014_4.jpg",
     scores: [3, 4, 4, 2, 2, 3, 5, 5, 1, 1]
 },
 {
-    name: "Mario",
-    photo: "https://en.wikipedia.org/wiki/Mario_Lopez#/media/File:Mario_Lopez_by_Gage_Skidmore.jpg",
+    name: "Bugs",
+    photo: "https://upload.wikimedia.org/wikipedia/en/thumb/1/17/Bugs_Bunny.svg/200px-Bugs_Bunny.svg.png",
     scores: [2, 3, 4, 4, 5, 5, 1, 1, 2, 3]
 },
 {
     name: "Donald",
-    photo: "https://en.wikipedia.org/wiki/Donald_Trump#/media/File:Donald_Trump_official_portrait.jpg",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Donald_Trump_official_portrait.jpg/946px-Donald_Trump_official_portrait.jpg",
     scores: [2, 2, 3, 3, 4, 1, 4, 5, 5, 1]
 },
 {
     name: "Bernie",
-    photo: "https://en.wikipedia.org/wiki/Bernie_Sanders#/media/File:Bernie_Sanders.jpg",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/d/de/Bernie_Sanders.jpg",
     scores: [2, 1, 2, 3, 3, 4, 4, 5, 5, 1]
 },
 {
     name: "Hillary",
-    photo: "https://upload.wikimedia.org/wikipedia/commons/2/27/Hillary_Clinton_official_Secretary_of_State_portrait_crop.jpg",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Hillary_Clinton_official_Secretary_of_State_portrait_crop.jpg/1280px-Hillary_Clinton_official_Secretary_of_State_portrait_crop.jpg",
     scores: [2, 1, 1, 2, 3, 3, 4, 4, 5, 5]
 }];
 
